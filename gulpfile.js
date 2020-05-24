3914d564b01a051f31b5c13ba8f2cf2940fe1239
Mehmet Guler <mehmetgspace@gmail.com>
2020-05-24 20:45:17 +0300

    Add gulp-sass package

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+function style() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/'))
+}
+
+exports.style = style;
